f1d8c46a35365a88fc04b3510fbc6dcbd55ab446
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-02-05 16:48:47 -0300

    chai testing done

--- test/math.spec.js
+++ test/math.spec.js
@@ -4,9 +4,10 @@
  */
 const assert = require('assert');
 const Math = require('../src/math.js');
+const expect = require('chai').expect;
 
 let value = 0;
-
+//-> Com chai
 describe('Math class', function() {
 
     // hooks
@@ -20,7 +21,7 @@ describe('Math class', function() {
         value = 5;
 
         math.asyncSum(value, 5, value => {
-            assert.strictEqual(value, 10);
+            expect(value).to.equal(10);
      
             done();
         });
@@ -31,6 +32,35 @@ describe('Math class', function() {
     it('Multiply two numbers', function(){
         const math = new Math();
 
-        assert.strictEqual(math.multiply(value, 5), 25);// aqui não vai passar pois o value será 0
+        expect(math.multiply(value, 5)).to.equal(0);// aqui não vai passar pois o value será 0
     });
 });
+
+// -> Com assert
+// describe('Math class', function() {
+
+//     // hooks
+//     beforeEach(function() {
+//         value = 0;
+//     })
+//     it('Sum two numbers', function (done) {
+//         const math = new Math();
+//         this.timeout(3000);
+
+//         value = 5;
+
+//         math.asyncSum(value, 5, value => {
+//             assert.strictEqual(value, 10);
+     
+//             done();
+//         });
+//     });
+// // utilizando on it.only apenas este teste irá ser executado 
+// // se utilizarmos o it.skip o teste será ignorado
+
+//     it('Multiply two numbers', function(){
+//         const math = new Math();
+
+//         assert.strictEqual(math.multiply(value, 5), 25);// aqui não vai passar pois o value será 0
+//     });
+// });
